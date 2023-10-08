0c656f625c7499d137fda37872826827c1412207
Mattia Di Eleuterio <madiele92@gmail.com>
2023-10-08 14:54:16 +0200

    add lsp keymaps ovverides

---
+++ overides/plugins/lsp-config.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		init = function()
+			local keymaps = require("lazyvim.plugins.lsp.keymaps")
+			local keys = keymaps.get()
+			keys[#keys + 1] = { "gd", vim.lsp.buf.definition, desc = "Goto Definition" }
+			keys[#keys + 1] = { "gn", vim.lsp.buf.incoming_calls, desc = "Incoming Calls" }
+			keys[#keys + 1] = { "gi", vim.lsp.buf.implementation, desc = "Goto Implementation" }
+			keys[#keys + 1] = { "gt", vim.lsp.buf.type_definition, desc = "Goto Type Definition" }
+			keys[#keys + 1] = { "gl", vim.lsp.codelens.run, desc = "Run codelens" }
+			keys[#keys + 1] = { "gR", vim.lsp.buf.rename, desc = "Rename Symbol" }
+			keys[#keys + 1] = { "gk", vim.lsp.buf.signature_help, desc = "Signature Help" }
+			keys[#keys + 1] = { "ga", vim.lsp.buf.code_action, desc = "Code Action" }
+		end,
+	},
+}
