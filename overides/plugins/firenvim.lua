af3137fe3589fe30c8ee223ad50cffb753f2f342
Mattia Di Eleuterio <madiele92@gmail.com>
2023-10-14 10:40:02 +0200

    firenvim font

--- overides/plugins/firenvim.lua
+++ overides/plugins/firenvim.lua
@@ -1,10 +1,28 @@
 return {
-	"glacambre/firenvim",
+  "glacambre/firenvim",
 
-	-- Lazy load firenvim
-	-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-	lazy = not vim.g.started_by_firenvim,
-	build = function()
-		vim.fn["firenvim#install"](0)
-	end,
+  -- Lazy load firenvim
+  -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+  lazy = not vim.g.started_by_firenvim,
+  build = function()
+    vim.fn["firenvim#install"](0)
+  end,
+  init = function()
+    vim.g.firenvim_config = {
+      globalSettings = {
+        alt = "all",
+      },
+      localSettings = {
+        [".*"] = {
+          cmdline = "neovim",
+          content = "text",
+          priority = 0,
+          selector = "textarea",
+          takeover = "never",
+        },
+      },
+    }
+    vim.cmd("set guifont=monospace:h6")
+  end,
 }
+
